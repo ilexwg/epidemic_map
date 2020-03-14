05335be88eeef1b2e6778c6c305cda7075de280b
ilexwg <ilexwg@gmail.com>
2020-03-14 19:18:54 +0800

    feat: Init project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">疫情地图</router-link> |
+      <router-link to="/about">疫情动态</router-link>
     </div>
     <router-view/>
   </div>
